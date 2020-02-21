53cb2b4f88c76eaea7604fe3486fa5c4d7de2d2d
Gremlin4544 <alve@mckethen.com>
2020-02-21 11:31:53 -0500

    added yarn to smurfs and add store to index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -7,6 +7,6 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-
+const store = createStore(applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
